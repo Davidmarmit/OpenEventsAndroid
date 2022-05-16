dcb918a777459c541736f719b7025dd82a5dab2b
David <davidmartinezmit@gmail.com>
2022-05-16 12:49:48 +0200

    Alert Dialog al sortir, mantenir sesió o tancar-la

--- app/src/main/java/com/openevents/HomeActivity.java
+++ app/src/main/java/com/openevents/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.openevents;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,8 +10,46 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HomeActivity extends AppCompatActivity {
 
     @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Cerrar OpenEvents")
+                .setMessage("¿Seguro que quieres cerrar la aplicación?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new AlertDialog.Builder(HomeActivity.this)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setTitle("Mantener sesión")
+                                .setMessage("¿Quieres mantener la sesión abierta?")
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        SharedPreferences sharedPreferences = getSharedPreferences("token", MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                                        editor.clear();
+                                        editor.apply();
+                                        finish();
+                                    }
+                                })
+                                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        finish();
+                                    }
+                                })
+                                .show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
 }
